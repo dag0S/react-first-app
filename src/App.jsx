3fd180a3a230ccae4c396ac790ffb50eadae60e0
Gosudarev Danila <danidagosudarev@gmail.com>
2023-10-27 22:12:10 +0300

    1-19

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CostItem from "./components/CostItem/CostItem"
+
+function App() {
+  return (
+    <div>
+      <h1>Начнем изучение React</h1>
+      <CostItem></CostItem>
+    </div>
+  )
+}
+
+export default App
